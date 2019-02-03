ae5ff7cef58cacab40d7a79bbef44a23b67db1a5
SupahAmbition <tylerbeverley@gmail.com>
2019-02-02 20:54:24 -0600

    display results of api request

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 import sys
+import requests
+
+APIkey = "jcgx5oyxczp630vm6vu9isb7vfar3z"
 
 
-print "arg[1] is: %s\n", sys.argv[1]
+UPC = sys.argv[1] 
+print "UPC is %s\n" %  UPC
 
 
+
+response = requests.get("https://api.barcodelookup.com/v2/products?barcode=715756100019&key=jcgx5oyxczp630vm6vu9isb7vfar3z")
+
+print "Status code is %d\n" % response.status_code 
+print "Result is %s\n" % response.content 
