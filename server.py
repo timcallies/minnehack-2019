6ab3b0001a2f55e922e59c0894bfbfd753387be3
SupahAmbition <tylerbeverley@gmail.com>
2019-02-02 23:14:01 -0600

    working on getting json data

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import json
 APIkey = "jcgx5oyxczp630vm6vu9isb7vfar3z"
 
 UPC = sys.argv[1] 
-print "UPC is %s\n" %  UPC
+print "UPC is %s" %  UPC
 
 #function that takes the UPC code and returns the result of the request. 
 def createRequest( upc ):
@@ -14,12 +14,11 @@ def createRequest( upc ):
     return requests.get(url, params=parameters, timeout = 5)
     
 
-
 response = createRequest( UPC )
 
-print "Status code is %d\n" % response.status_code 
-print "Result is %s\n" % response.content 
+print "Status code is %d" % response.status_code 
 
-
+json = response.json()
+print "Brand is %s" % json["products"][0]["brand"]
 
 
