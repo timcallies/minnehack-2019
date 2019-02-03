5b1a3560bd7ebda495ce28b0a604c395a6d1d14c
SupahAmbition <tylerbeverley@gmail.com>
2019-02-03 03:24:51 -0600

    started gelocation code

--- testMap.py
+++ testMap.py
@@ -1,3 +1,22 @@
+import geopy.distance 
+import requests 
+
+APIkey = "AIzaSyBZ_aIJmoMpqTvqfVVcwkJ11lK8QYLA35M"
+
+
+def calcDistance( lat1, lon1, lat2, lon2):
+    coords_1 = (lat1, lon1)
+    coords_2 = (lat2, lon2)
+
+    return geopy.distance.vincenty(coords_1, coords_2).miles
+
+def geocode( addressStr):
+    url = "https://maps.googleapis.com/maps/api/geocode/json"
+    parameters = {"address": addressStr, "key":APIkey }
+    return requests.get(url, params=parameters, timeout = 5) 
+
+
+
 manufacturers = { 'arizona' : ['Woodbury, NY'],
                   'driscolls' : ['Santa Maria, CA', 'Dover, FL'],
                   'dole' : ['Wahiawa, HI', 'San Jose, Costa Rice', 'Medellin, Colombia'],
@@ -8,8 +27,7 @@ manufacturers = { 'arizona' : ['Woodbury, NY'],
                   'tyson' : ['Albertville, AL', 'Blountsville, AL', 'Berryville, AR', 'Dexter, MO', 'New Holland, PA']
                   }
  
-print(manufacturers['dole'][1])
-print(manufacturers['tyson'][4])
-
-
+# st. paul = 44.9537 93.0900
+print "%d" % calcDistance( 44.9537, 93.0900, 44.9591, 89.6301) 
                         
+
