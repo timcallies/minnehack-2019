a478606a38229b4b4c896d91e1794168fd107d17
Tim Callies <call3675@stthomas.edu>
2019-02-02 23:04:42 -0600

    Created base for webserver.

---
+++ scripts/camera.js
@@ -0,0 +1,10 @@
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
+}
+
+if (hasGetUserMedia()) {
+  // Good to go!
+} else {
+  alert('getUserMedia() is not supported by your browser');
+}
