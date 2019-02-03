2a06d8853667d25766d13005017e0d14c7966009
SupahAmbition <tylerbeverley@gmail.com>
2019-02-03 07:13:35 -0600

    updated manufacturer names

--- geoloc.py
+++ geoloc.py
@@ -7,8 +7,8 @@ import pickle
 APIkey = "AIzaSyBZ_aIJmoMpqTvqfVVcwkJ11lK8QYLA35M"
 
 manufacturers = { "Ferolito Vultaggio & Sons" : ['One Arizona Plaza, 60 Crossways Park Drive, Suite 400, Woodbury, NY 11797'],
-                  'driscolls' : ['S E St, Santa Maria, CA 93455', '12880 US-92, Dover, FL 33527'],
-                  'dole' : ['1116 Whitmore Ave, Wahiawa, HI 96786', 'San Jose, Costa Rice', 'Medellin, Colombia'],
+                "driscoll's" : ['S E St, Santa Maria, CA 93455', '12880 US-92, Dover, FL 33527'],
+                "dole" : ['1116 Whitmore Ave, Wahiawa, HI 96786', 'San Jose, Costa Rice', 'Medellin, Colombia'],
                   }
 
 
