54e84d87afd80d367ffe98ea98f9880c4d9d7163
SupahAmbition <tylerbeverley@gmail.com>
2019-02-02 22:22:58 -0600

    created function that creates the api call with the input from the user

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
 import sys
 import requests
+import json 
 
 APIkey = "jcgx5oyxczp630vm6vu9isb7vfar3z"
-
 
 UPC = sys.argv[1] 
 print "UPC is %s\n" %  UPC
 
+#function that takes the UPC code and returns the result of the request. 
+def createRequest( upc ):
+    url = "https://api.barcodelookup.com/v2/products"
+    parameters = {"barcode": upc, "key": APIkey } 
+    return requests.get("https://api.barcodelookup.com/v2/products", params=parameters, timeout = 5)
+    
 
 
-response = requests.get("https://api.barcodelookup.com/v2/products?barcode=715756100019&key=jcgx5oyxczp630vm6vu9isb7vfar3z")
+response = createRequest( UPC )
 
 print "Status code is %d\n" % response.status_code 
 print "Result is %s\n" % response.content 
+
+
+
+
