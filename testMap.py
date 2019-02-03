63e186177146b895e267c5bd077cf8ad94078edb
abates1211 <bate2807@stthomas.edu>
2019-02-03 01:55:39 -0600

    created a map for locations

---
+++ testMap.py
@@ -0,0 +1,15 @@
+manufacturers = { 'arizona' : ['Woodbury, NY'],
+                  'driscolls' : ['Santa Maria, CA', 'Dover, FL'],
+                  'dole' : ['Wahiawa, HI', 'San Jose, Costa Rice', 'Medellin, Colombia'],
+                  'hunts' : ['Oakdale, CA'],
+                  'green giant' : ['Minnesota', 'Idaho', 'Wisconsin', 'New York', 'Peru'],
+                  'bush brothers & company' : ['Augusta, WI', 'Chestnut Hill, TN'],
+                  'idahoan foods' : ['Idaho Falls, ID'],
+                  'tyson' : ['Albertville, AL', 'Blountsville, AL', 'Berryville, AR', 'Dexter, MO', 'New Holland, PA']
+                  }
+ 
+print(manufacturers['dole'][1])
+print(manufacturers['tyson'][4])
+
+
+                        
